fe3a181e2287bf3f1cf4d06572190d6f94a1420e
rachel doran <rmitchell0925@gmail.com>
2019-08-15 20:36:03 -0400

    modify

--- assets/app.js
+++ assets/app.js
@@ -1,9 +1,11 @@
-var timeRemaing = 30;
+var setTimeout = 30;
 
 setTimeout(thirtySeconds, 1000 * 3);
 
 function thirtySeconds() {
     // in the element with an id of time-left add an h2 saying About 10 Seconds Left!
     // console log 10 seconds left
-    $("#time").append("<h2>Time Remaining</h2>");
+    $("#timer").append("<h2>Time Remaining</h2>");
+    
+}
   
