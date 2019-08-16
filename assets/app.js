3c45507430d0245df06373283ff12625a21fad99
rachel doran <rmitchell0925@gmail.com>
2019-08-15 20:02:12 -0400

    modify

--- assets/app.js
+++ assets/app.js
@@ -1,51 +1,10 @@
 var time = 30;
 
-var intervalId;
+function start (){
+    interval=setInterval(decrement, 30000);
 
-    //  When the stop button gets clicked, run the stop function.
-    $("#stop").on("click", stop);
-
-    //  When the resume button gets clicked, execute the run function.
-    $("#resume").on("click", run);
-
-    //  The run function sets an interval
-    //  that runs the decrement function once a second.
-    //  *****BUG FIX******** 
-    //  Clearing the intervalId prior to setting our new intervalId will not allow multiple instances.
-    function run() {
-      clearInterval(intervalId);
-      intervalId = setInterval(decrement, 30000);
+    function decrement (){
+        time--;
+        $("#time").html("<h2> Time Remaining" + time "</h2>")
     }
-
-    //  The decrement function.
-    function decrement() {
-
-      //  Decrease number by one.
-      time--;
-
-      //  Show the number in the #show-number tag.
-      $("#show-time").html("<h2>" + time + "</h2>");
-
-
-      //  Once number hits zero...
-      if (time === 0) {
-
-        //  ...run the stop function.
-        stop();
-
-        //  Alert the user that time is up.
-        alert("Time Up!");
-      }
-    }
-
-    //  The stop function
-    function stop() {
-
-      //  Clears our intervalId
-      //  We just pass the name of the interval
-      //  to the clearInterval function.
-      clearInterval(intervalId);
-    }
-
-    //  Execute the run function.
-    run();
+}
