33f3462ddf80a79ae691db090f88acf51b68f8f9
rachel doran <rmitchell0925@gmail.com>
2019-08-17 16:33:50 -0400

    modify

--- assets/app.js
+++ assets/app.js
@@ -10,7 +10,7 @@ $(document).ready(function () {
   
   $(".timer").text("Time Remaining: " + time);
   $(".wins").html("Wins: " + winner)
-  $(".losses").html("Losses " + loser)
+  $(".losses").html("Losses: " + loser)
 
   var intervalId = setInterval(count, 3000)
   function count() {
@@ -26,66 +26,81 @@ $(document).ready(function () {
 
   //Question One//
   $("#bob-dylan").click(function () {
+   $(".wins").text("Wins: " + winner)
+   alert("Hell Yeah!");
     winner++;
-    alert("Hell Yeah!");
     console.log(winner)
   });
 
   $("#robert-plant").click(function () {
+    $(".losses").text("Losses: " + loser)
     alert("Hell No!");
     loser++;
     console.log(loser)
     
   });
   $("#paul-mccartney").click(function () {
+    $(".losses").text("Losses: " + loser)
     alert("Hell No!");
     loser++;
   });
 
   //Question Two//
   $("#nirvana").click(function () {
+    $(".losses").text("Losses: " + loser)
     alert("Hell No!");
     loser++;
   });
   
   $("#rem").click(function () {
-    winner++;
+    $(".wins").text("Wins: " + winner)
     alert("Hell Yeah!");
+    winner++;
+  
   });
 
   $("#pumpkins").click(function () {
+    $(".losses").text("Losses: " + loser)
     alert("Hell No!");
     loser++;
   });
 
 //Question Three//
 $("#little-sister").click(function () {
+  $(".losses").text("Losses: " + loser)
   alert("Hell No!");
   loser++;
 });
 
 $("#heartbreak").click(function () {
-  winner++;
+  $(".wins").text("Wins: " + winner)
   alert("Hell Yeah!");
+  winner++;
+
 });
 
 $("#jailhouse").click(function () {
+  $(".losses").text("Losses: " + loser)
   alert("Hell No!");
   loser++;
 });
 
 //Question Four//
 $("#beatles").click(function () {
- winner++;
+  $(".wins").text("Wins: " + winner)
   alert("Hell Yeah!");
+  winner++;
+  
 });
 
 $("#stones").click(function () {
+  $(".losses").text("Losses: " + loser)
   alert("Hell No!");
   loser++;
 });
 
 $("#zeppelin").click(function () {
+  $(".losses").text("Losses: " + loser)
   alert("Hell No!");
   loser++;
 });
